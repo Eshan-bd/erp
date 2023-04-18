4c56904fe4554dc2a1a9d7659798f6d91a33efbf
M Sakib Osman Eshan <Sakib.Osman@brainstation-23.com>
2023-04-19 01:20:48 +0600

    Added employee crud.

---
+++ src/main/java/com/brainstation23/erp/controller/web/EmployeesController.java
@@ -0,0 +1,38 @@
+package com.brainstation23.erp.controller.web;
+
+import com.brainstation23.erp.constant.ROLE;
+import com.brainstation23.erp.mapper.UserMapper;
+import com.brainstation23.erp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@ControllerAdvice
+@RequestMapping
+public class EmployeesController {
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @GetMapping("/employees")
+    public String users(Model model){
+
+        log.info("Getting List of employees");
+        var domains = userService.getAllByRole(PageRequest.of(0, 20), ROLE.EMPLOYEE);
+        model.addAttribute("employeeList", domains);
+
+        var response = ResponseEntity.ok(domains.map(userMapper::domainToResponse));
+        if (response.getStatusCode().isError()){
+            return response.toString();
+        }
+        return "employees";
+    }
+}
